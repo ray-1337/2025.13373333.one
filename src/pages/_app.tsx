34f90cae7bea4487ca3cfe2b1b3cca83cadbdd3a
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-07 07:09:29 +0800

    wrap App component with MantineProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { Fragment } from "react";
 import Head from "next/head";
 import type { AppProps } from "next/app";
+import { MantineProvider, createTheme } from "@mantine/core";
 
 import "normalize.css";
 
+import "@mantine/core/styles.css";
+
+
+const mantineTheme = createTheme({
+  fontFamily: "var(--authenticFontFamily)"
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,7 +19,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,701&display=swap" rel="preload stylesheet" as={"style"} />
       </Head>
 
-      <Component {...pageProps} />
+      <MantineProvider theme={mantineTheme} forceColorScheme={"dark"}>
+        <Component {...pageProps} />
+      </MantineProvider>
     </Fragment>
   );
 };
