5f6c6e4b6a210a6c392ac051f0f2e445cc0d20ac
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-07 07:13:26 +0800

    bring left and right arrow from Tabler

---
+++ src/icons/LeftArrow.tsx
@@ -0,0 +1,10 @@
+export default function LeftArrow() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--authenticDarkColor001)" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M5 12l14 0" />
+      <path d="M5 12l6 6" />
+      <path d="M5 12l6 -6" />
+    </svg>
+  );
+};
---
+++ src/icons/RightArrow.tsx
@@ -0,0 +1,10 @@
+export default function RightArrow() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M5 12l14 0" />
+      <path d="M13 18l6 -6" />
+      <path d="M13 6l6 6" />
+    </svg>
+  );
+};
