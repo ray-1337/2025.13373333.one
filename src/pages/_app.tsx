ffb7c2c5476a25ab97789fa376d2c27f47dbdbf3
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-07 07:09:36 +0800

    set a default viewport meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
+        <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, shrink-to-fit=no, initial-scale=1.0" />
+
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,701&display=swap" rel="preload stylesheet" as={"style"} />
       </Head>
 
