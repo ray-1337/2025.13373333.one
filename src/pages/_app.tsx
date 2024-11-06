a646f7871633e860ae68852a024d9241a41d40be
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-06 16:16:14 +0800

    import normalize.css to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from "next/app";
 
+import "normalize.css";
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 };
