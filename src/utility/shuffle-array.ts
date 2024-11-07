ae93e36efeba09f08009815efdd70e30c0167fd3
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-07 08:02:02 +0800

    add a function to shuffle arrays

---
+++ src/utility/shuffle-array.ts
@@ -0,0 +1,11 @@
+export default function shuffleArray<R>(array: Array<R>) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    
+    array[i] = array[j];
+    array[j] = temp;
+  };
+
+  return array;
+};
