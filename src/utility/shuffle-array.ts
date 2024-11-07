b8bbbb4b11253137dd1dc40722373b211ab88026
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-07 08:10:34 +0800

    let -> const

--- src/utility/shuffle-array.ts
+++ src/utility/shuffle-array.ts
@@ -1,7 +1,7 @@
 export default function shuffleArray<R>(array: Array<R>) {
   for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let temp = array[i];
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
     
     array[i] = array[j];
     array[j] = temp;
