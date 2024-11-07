d55dd0ba68e87ddc3a083075394b2900b1ce4217
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-07 08:03:44 +0800

    import Biography to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import dynamic from "next/dynamic";
 // style
 import style from "@/styles/components/Primary.module.css";
 
+const Biography = dynamic(() => import("@/components/Biography"), { ssr: false });
 const Interface = dynamic(() => import("@/components/Interface"), { ssr: false });
 const Projects = dynamic(() => import("@/components/Projects"), { ssr: false });
 
@@ -15,6 +16,11 @@ export default function MainPage() {
   return (
     <section className={style["main-root"]}>
       <MenuContext.Provider value={[menuState, setMenuState]}>
+        <section className={style["main-insider-root"]}>
+          {/* biography */}
+          <Biography />
+        </section>
+
         <Interface active={menuState !== null}>
           {
             menuState === "projects" && <Projects />
