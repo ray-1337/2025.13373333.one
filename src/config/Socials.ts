dad0bfde27df54f482a67c29cada51f7070600ff
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-12-24 01:12:21 +0800

    change linkedin logo to self-hosted one

--- src/config/Socials.ts
+++ src/config/Socials.ts
@@ -7,7 +7,7 @@ const Socials: Array<SocialsProp> = [
   },
   {
     url: "https://www.linkedin.com/in/ray1337/",
-    iconURL: "https://cdn.simpleicons.org/linkedin/white"
+    iconURL: "https://saint-chroma.cdn.13373333.one/0001/dumpster/linkedin-white.svg"
   }
 ];
 
